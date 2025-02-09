4dcfa9c30980ef6484bad9e0420a92b10f732ac6
Mauricio Aldana <mauricio.aldana@unmsm.edu.pe>
2025-02-08 22:44:20 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgClass, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgFor,NgIf,NgClass,NgSwitch,NgSwitchCase,NgSwitchDefault],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Facebook';
+  nombre: string = 'Mauricio';
+  imagen = `https://robohash.org/${this.nombre}`;
+  colores: string[] = ['rojo', 'verde', 'azul', 'amarillo'];
+  haynotificacion: boolean = true;
+  colorBackground: string = 'purple';
+  userRole: string = 'admin';
 }
